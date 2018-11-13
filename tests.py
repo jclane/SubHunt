458729614518f93b69db08862ec3d69c76d5e6ce
Justin Lane <jclane@users.noreply.github.com>
2018-11-13 10:16:34 -0500

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,45 +1,46 @@
 from backend import *
 
 
-part = ["111","PC4-19200S","Lenovo","SO-DIMM","8GB","8GB DDR4 2400 SoDIMM","FALSE","TRUE"]
-
-conn = create_connection()
-
 def import_test():
     """Test import from csv"""
     import_from_csv(r".\import\mem_test.csv")
+    import_from_csv(r".\import\hdd_test.csv")
     try:
         assert part_in_db("mem_test", "123") == True
         assert part_in_db("mem_test", "456") == True
         assert part_in_db("mem_test", "789") == True
-        remove_part("mem_test", "123")
-        remove_part("mem_test", "456")
-        remove_part("mem_test", "789")
-        assert part_in_db("mem_test", "123") == False
-        assert part_in_db("mem_test", "456") == False
-        assert part_in_db("mem_test", "789") == False
+        assert part_in_db("hdd_test", "1111111") == True
+        assert part_in_db("hdd_test", "1111112") == True
+        assert part_in_db("hdd_test", "2222222") == True
+        assert part_in_db("hdd_test", "2222223") == True
         print("Import: Passed!\n")
     except AssertionError as e:
         print("Import: Failed!\n")
         print(str(e))
         
 
-def add_mem_test():
+def add_part_test():
     """Test adding parts to database"""
-    add_part("mem_test", part)
+    part_mem = ["111","PC4-19200S","Lenovo","SO-DIMM","8GB","8GB DDR4 2400 SoDIMM","FALSE","TRUE"]
+    part_hdd = ["111","Lenovo","SATA","500","","7200","HDD","2.5","7","SATA III","","FALSE","TRUE"]
+    add_part("mem_test", part_mem)
+    add_part("hdd_test", part_hdd)
     try:
         assert part_in_db("mem_test", "111") == True
+        assert part_in_db("hdd_test", "111") == True
         print("Add part: Passed!\n")
     except AssertionError as e:
         print("Add part:Failed!\n")
         print(str(e))
 
 
-def remove_mem_test():
+def remove_part_test():
     """Test removed parts from database"""
     remove_part("mem_test", "111")
+    remove_part("hdd_test", "111")
     try:
         assert part_in_db("mem_test", "111") == False
+        assert part_in_db("hdd_test", "111") == False
         print("Remove part: Passed!\n")
     except AssertionError as e:
         print("Remove part: Failed!\n")
@@ -48,19 +49,18 @@ def remove_mem_test():
         
 def list_subs_test():
     """Test checking listing of subs"""
-    import_from_csv(r".\import\hdd.csv")
     part_needing_sub = ["111","Acer","SATA","500","1000","5400","SSHD","2.5","7","SATA III","","FALSE","TRUE"]
     sub = ["112","Acer","SATA","500","1000","5400","SSHD","2.5","7","SATA III","","FALSE","TRUE"]
     also_a_sub = ["113","CVO","SATA","500","1000","5400","SSHD","2.5","7","SATA III","","FALSE","TRUE"]
     not_a_sub = ["222","Acer","SATA","500","","5400","SSHD","2.5","7","SATA III","","TRUE","FALSE"]
     also_not_a_sub = ["223","Acer","SATA","","500","5400","SSD","2.5","7","SATA III","","FALSE","FALSE"]
     
-    add_part("hdd", part_needing_sub)
-    add_part("hdd", sub)
-    add_part("hdd", also_a_sub)
-    add_part("hdd", not_a_sub)
-    add_part("hdd", also_not_a_sub)
-    subs = list_subs("hdd", part_needing_sub[0])
+    add_part("hdd_test", part_needing_sub)
+    add_part("hdd_test", sub)
+    add_part("hdd_test", also_a_sub)
+    add_part("hdd_test", not_a_sub)
+    add_part("hdd_test", also_not_a_sub)
+    subs = list_subs("hdd_test", part_needing_sub[0])
     try:
         assert any(lst[1] == "112" for lst in subs) == True
         assert any(lst[1] == "113" for lst in subs) == True
@@ -69,68 +69,66 @@ def list_subs_test():
         print("List subs: Passed!\n")
     except AssertionError as e:
         print("List subs: Failed!\n")
-        print(str(e))
+        print(repr(e))
 
         
 def valid_sub_test():
     """Test checking if subs are valid"""
-    import_from_csv(r".\import\hdd.csv")
     part_needing_sub = ["111","Acer","SATA","500","1000","5400","SSHD","2.5","7","SATA III","","FALSE","TRUE"]
     sub = ["112","Acer","SATA","500","1000","5400","SSHD","2.5","7","SATA III","","FALSE","TRUE"]
     also_a_sub = ["113","CVO","SATA","500","1000","5400","SSHD","2.5","7","SATA III","","FALSE","TRUE"]
     not_a_sub = ["222","Acer","SATA","500","","5400","SSHD","2.5","7","SATA III","","TRUE","FALSE"]
     also_not_a_sub = ["223","Acer","SATA","","500","5400","SSD","2.5","7","SATA III","","FALSE","FALSE"]
     
-    add_part("hdd", part_needing_sub)
-    add_part("hdd", sub)
-    add_part("hdd", also_a_sub)
-    add_part("hdd", not_a_sub)
-    add_part("hdd", also_not_a_sub)
+    add_part("hdd_test", part_needing_sub)
+    add_part("hdd_test", sub)
+    add_part("hdd_test", also_a_sub)
+    add_part("hdd_test", not_a_sub)
+    add_part("hdd_test", also_not_a_sub)
     try:
-        assert is_valid_sub("hdd", "111", "112") == True
-        assert is_valid_sub("hdd", "112", "113") == True
-        assert is_valid_sub("hdd", "111", "222") == False
-        assert is_valid_sub("hdd", "113", "223") == False
-        assert is_valid_sub("hdd", "222", "223") == False
+        assert is_valid_sub("hdd_test", "111", "112") == True
+        assert is_valid_sub("hdd_test", "112", "113") == True
+        assert is_valid_sub("hdd_test", "111", "222") == False
+        assert is_valid_sub("hdd_test", "113", "223") == False
+        assert is_valid_sub("hdd_test", "222", "223") == False
         print("Valid sub: Passed!\n")
     except AssertionError as e:
         print("Valid sub: Failed!\n")
-        print(str(e))        
+        print(repr(e))        
         
 
 def edit_part_test():
-
+    """Test checking if parts are updated correctly"""
     part_num = ["TEST", "999", "999", "999", "999", "999", "999", "999", "999", "999", "999", "TRUE", "TRUE"]
     update_part_num = ["TEST", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "FALSE", "FALSE"]
     
-    add_part("hdd", part_num)
-    update_part("hdd", update_part_num)
-    updated_part = [value for key, value in convert_to_dict("hdd", "TEST").items()]
+    add_part("hdd_test", part_num)
+    update_part("hdd_test", update_part_num)
+    updated_part = [value for key, value in convert_to_dict("hdd_test", "TEST").items()]
     try:
         assert updated_part == update_part_num
-        print("Edit part: Passed!")
+        print("Edit part: Passed!\n")
     except AssertionError as e:
-        print("Edit part: Failed!")
+        print("Edit part: Failed!\n")
         print(repr(e))
     
     
 def remove_table_test():
+    """Test checking if tables are removed correctly"""
     try:
-        assert remove_table("hdd") == True
-        assert remove_table("mem") == True
-        assert remove_table("cpu") == True
+        assert remove_table("hdd_test") == True
         assert remove_table("mem_test") == True
+        assert remove_table("cpu_test") == True
         print("Remove table: Passed!\n")
     except AssertionError as e:
         print("Remove table: Failed!\n")
-        print(str(e))
+        print(repr(e))
      
                 
 import_test()
-add_mem_test()
-remove_mem_test()    
+add_part_test()
+remove_part_test()    
 list_subs_test()
 valid_sub_test()
 edit_part_test()
 remove_table_test()
-close_connection(conn)
